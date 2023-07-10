087d7860f24bc3a4519a7342f67d25672869355e
franconero14 <nerobalor@gmail.com>
2023-07-10 21:32:09 +0700

    add method back form 2

--- FinalProjectKel7/Form2.cs
+++ FinalProjectKel7/Form2.cs
@@ -16,5 +16,12 @@ namespace FinalProjectKel7
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
     }
 }
