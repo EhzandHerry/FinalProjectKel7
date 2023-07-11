7e1f85c29862b952b1f5d08877bb979aab1c3eb5
franconero14 <nerobalor@gmail.com>
2023-07-11 13:49:12 +0700

    add method function button save and clear for text box id_gedung form ruangan

--- FinalProjectKel7/ruangan.Designer.cs
+++ FinalProjectKel7/ruangan.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FinalProjectKel7
 {
-    partial class ruangan
+    partial class r1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rg = new System.Windows.Forms.TextBox();
+            this.btnview = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnc = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnsv = new System.Windows.Forms.Button();
+            this.ng = new System.Windows.Forms.TextBox();
+            this.ig = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rg
+            // 
+            this.rg.Location = new System.Drawing.Point(253, 347);
+            this.rg.Name = "rg";
+            this.rg.Size = new System.Drawing.Size(214, 26);
+            this.rg.TabIndex = 52;
+            // 
+            // btnview
+            // 
+            this.btnview.Location = new System.Drawing.Point(492, 77);
+            this.btnview.Name = "btnview";
+            this.btnview.Size = new System.Drawing.Size(87, 35);
+            this.btnview.TabIndex = 51;
+            this.btnview.Text = "View";
+            this.btnview.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(227, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 50;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(509, 254);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 35);
+            this.button4.TabIndex = 49;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnc
+            // 
+            this.btnc.Location = new System.Drawing.Point(617, 254);
+            this.btnc.Name = "btnc";
+            this.btnc.Size = new System.Drawing.Size(87, 35);
+            this.btnc.TabIndex = 48;
+            this.btnc.Text = "Clear";
+            this.btnc.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(617, 301);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(87, 35);
+            this.btnadd.TabIndex = 47;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // btnsv
+            // 
+            this.btnsv.Location = new System.Drawing.Point(509, 301);
+            this.btnsv.Name = "btnsv";
+            this.btnsv.Size = new System.Drawing.Size(87, 35);
+            this.btnsv.TabIndex = 46;
+            this.btnsv.Text = "Save";
+            this.btnsv.UseVisualStyleBackColor = true;
+            // 
+            // ng
+            // 
+            this.ng.Location = new System.Drawing.Point(253, 301);
+            this.ng.Name = "ng";
+            this.ng.Size = new System.Drawing.Size(214, 26);
+            this.ng.TabIndex = 45;
+            // 
+            // ig
+            // 
+            this.ig.Location = new System.Drawing.Point(253, 263);
+            this.ig.Name = "ig";
+            this.ig.Size = new System.Drawing.Size(214, 26);
+            this.ig.TabIndex = 44;
+            this.ig.TextChanged += new System.EventHandler(this.ig_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Ruangan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Nama_Gedung";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Id_Gedung";
+            // 
+            // r1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rg);
+            this.Controls.Add(this.btnview);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnc);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.btnsv);
+            this.Controls.Add(this.ng);
+            this.Controls.Add(this.ig);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "r1";
             this.Text = "ruangan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox rg;
+        private System.Windows.Forms.Button btnview;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnc;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnsv;
+        private System.Windows.Forms.TextBox ng;
+        private System.Windows.Forms.TextBox ig;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- FinalProjectKel7/ruangan.cs
+++ FinalProjectKel7/ruangan.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,45 @@ using System.Windows.Forms;
 
 namespace FinalProjectKel7
 {
-    public partial class ruangan : Form
+    public partial class r1 : Form
     {
-        public ruangan()
+        private string stringConnection = "Data Source=LAPTOP-G2F55ONU\\EHZANDHERRY;" + "database=Act6;User ID=sa;Password=Conex999";
+        private SqlConnection koneksi;
+        public r1()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            refreshform();
+        }
+
+        private void refreshform()
+        {
+            ig.Text = "";
+            ng.Text = "";
+            rg.Text = "";
+            ig.Enabled = true;
+            ng.Enabled = true;
+            rg.Enabled = true;
+            btnsv.Enabled = false;
+            btnadd.Enabled = false;
+        }
+
+        private void dataGridView1_CellContentClick()
+        {
+            koneksi.Open();
+            string str = "select id_gedung, nama_gedung, ruangan From dbo.Ruangan";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataTable dataTable = new DataTable();
+            da.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            koneksi.Close();
+        }
+
+        private void ig_TextChanged(object sender, EventArgs e)
+        {
+            ig.Enabled = true;
+            btnsv.Enabled = true;
+            btnc.Enabled = true;
         }
     }
 }
